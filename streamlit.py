bffdc72e6fc4e647ff2ed09ed613d80caf80fa10
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-03 23:50:46 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -60,7 +60,7 @@ You challenge me to a game of Chess :chess_pawn: [@chess.com](https://friend.che
 st.divider()
 
 st.subheader("Certification")
-st.image([image3,image4,image5], width=175)
+st.image([image3,image4,image5], width=150)
 
 st.image([image6,image7,image8,image9], width=175)
 
