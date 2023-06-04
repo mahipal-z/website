d4c5588cbff3b2d939a5fa13c1fe8451015df4ad
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-04 04:52:04 -0400

    Update 3_ML.py

--- pages/3_ML.py
+++ pages/3_ML.py
@@ -1,1 +1,9 @@
+import streamlit as st
+
+st.title("Data Analysis and Business Intelligence")
+
+st.subheader("""Listed below are the data driven projects I have worked on. 
+             \nThis page gives an idea about how various business problems can be solved with the help of data science tools and technology.
+             """)
+
 
