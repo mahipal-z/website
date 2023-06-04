c556ff2ebe3d98f18744365ea77b391f95f18b27
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-04 06:25:17 -0400

    Update 2_Data_Analysis_and_Business_I.py

--- pages/2_Data_Analysis_and_Business_I.py
+++ pages/2_Data_Analysis_and_Business_I.py
@@ -5,5 +5,5 @@ st.title("Data Analysis and Business Intelligence")
 
 df1 = pd.read_excel("DA.xlsx", header=None, names=None)
 
-st.dataframe(data=df1, use_container_width=True, hide_index=True, column_config=None)
+st.dataframe(data=df1, height=1000, use_container_width=True, hide_index=True, column_config=None)
 
