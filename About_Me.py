a60fce16405df3449a78ca4eae4949507fdac786
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-07 23:20:27 -0400

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -25,7 +25,7 @@ st.set_page_config(
 #main page layout
 ######################
 
-col1, col2 = st.columns([0.7, 0.3], gap="large")
+col1, col2 = st.columns([0.9, 0.1], gap="large")
 
 with col1:
     st.title("About Me")
