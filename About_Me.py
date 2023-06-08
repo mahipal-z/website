ebe4e18b49b80e2ccc9e94da3c18624c965dbcef
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-07 23:23:27 -0400

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -25,7 +25,7 @@ st.set_page_config(
 #main page layout
 ######################
 
-col1, col2 = st.columns([0.9, 0.1], gap="large")
+col1, col2 = st.columns([0.7, 0.3], gap="large")
 
 with col1:
     st.title("About Me")
@@ -42,11 +42,16 @@ with col1:
     st.markdown("""\n""")
     st.markdown("""**Connect with me on** [**LinkedIn**](https://www.linkedin.com/in/mahipalzanakat) | [**GitHub**](https://github.com/mahipal-z) | [**Medium**](https://medium.com/@storiesbymahipal)
   """)
-    st.divider()
+    
+with col2:
+    st.subheader("**Mahipal Zanakat**")
+    st.image(image1)
+
+st.divider()
 
 
-    st.subheader("I could be a good company if")
-    st.markdown("""
+st.subheader("I could be a good company if")
+st.markdown("""
 You challenge me to a game of Chess :chess_pawn: [@chess.com](https://friend.chess.com/W7Xra)
 \nor Hiking :runner: nearby or trying a new restaurant :shallow_pan_of_food:
 \nor Bowling :bowling:, Lifting Weights :weight_lifter:, Card games, Board games :game_die:, or for Cooking
@@ -54,28 +59,27 @@ You challenge me to a game of Chess :chess_pawn: [@chess.com](https://friend.che
 """
 )
 
-    st.divider()
+st.divider()
 
-    st.subheader("Certification")
-    st.image([image3,image4,image5], width=200)
+st.subheader("Certification")
+st.image([image3,image4,image5], width=200)
 
-    st.image([image6,image7,image8,image9], width=170)
+st.image([image6,image7,image8,image9], width=170)
 
-    st.markdown("""**Validation Credentials** [**Credly**](https://www.credly.com/users/mahipal-zanakat) | [**DataCamp**](https://www.datacamp.com/certificate/DS0021765360075)""")
+st.markdown("""**Validation Credentials** [**Credly**](https://www.credly.com/users/mahipal-zanakat) | [**DataCamp**](https://www.datacamp.com/certificate/DS0021765360075)""")
 
 #components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="8e92d3b3-1cfa-4ad3-8173-a4c06bb239a2" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>"""
 #                ,height=250)
-    st.divider()
-    st.subheader("Education")
+st.divider()
+st.subheader("Education")
 
-    st.markdown("""**Master of Engineering, Mechanical | University of Ottawa**
+st.markdown("""**Master of Engineering, Mechanical | University of Ottawa**
          \n**Data Scientist in Python | DataCamp**
          \n**Data Analyst in SQL | DataCamp**
          \n**Machine Learning by Andrew Ng. | Stanford University**""")
 
-with col2:
-    st.title("**Mahipal Zanakat**")
-    st.image(image1)
+#with col2:
+    
 ######################
 #sidebar layout
 ######################
