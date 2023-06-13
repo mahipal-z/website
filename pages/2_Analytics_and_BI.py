830dc013fd3f7d636edd4a22c0e86344e99e46b5
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-12 23:11:12 -0400

    Update 2_Analytics_and_BI.py

--- pages/2_Analytics_and_BI.py
+++ pages/2_Analytics_and_BI.py
@@ -11,7 +11,7 @@ st.markdown('''
 \n**:green[Solution:]** Compared the sales dataset with the critic and user reviews to determine whether video games have improved as the gaming market has grown. Found answers to several business questions using joins, set theory, grouping, filtering, and ordering techniques.
 \n**:green[Impact:]** The findings from this analysis provides insights to developer team that helps them finalize the Game Design Document during the pre-production stage.
 \n**:green[Tools & Technology:]** Relational databases, PostgreSQL
-\n**:blue[Click here to see analysis](https://github.com/mahipal-z/Videogames-EDA/blob/main/notebook.ipynb)**
+\n[**:blue[Click here to see analysis]**](https://github.com/mahipal-z/Videogames-EDA/blob/main/notebook.ipynb)
 ''')
 
 
