e8bcc2026a628424428ecf42328750669d3aab0f
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-12 23:00:07 -0400

    Update 2_Data_Analysis_and_Business_I.py

--- pages/2_Data_Analysis_and_Business_I.py
+++ pages/2_Data_Analysis_and_Business_I.py
@@ -5,14 +5,13 @@ st.title("Data Analysis and Business Intelligence")
 st.divider()
 
 st.header(':blue[1. Exploratory data analysis of the top 400 video games released since 1977]')
-st.write('''
-### [1. Exploratory data analysis of the top 400 video games released since 1977](https://github.com/mahipal-z/Videogames-EDA/blob/main/notebook.ipynb)
-----
-<ins>__Problem Description:__</ins> The objective was to do a market research analysis of video games sector by identifying release years that users and critics liked best and exploring the business side of gaming by looking at game sales data.  
-<ins>__Problem Type:__</ins> __Exploratory Data Analysis (EDA)__  
-<ins>__Solution:__</ins> Compared the sales dataset with the critic and user reviews to determine whether video games have improved as the gaming market has grown. Found answers to several business questions using joins, set theory, grouping, filtering, and ordering techniques.  
-<ins>__Impact:__</ins> The findings from this analysis provides insights to developer team that helps them finalize the Game Design Document during the pre-production stage.  
-<ins>__Tools & Technology:__</ins>  __Relational databases__, __PostgreSQL__  
+st.markdown('''
+**:green[Problem Description:]** The objective was to do a market research analysis of video games sector by identifying release years that users and critics liked best and exploring the business side of gaming by looking at game sales data.
+\n**:green[Problem Type:]** Exploratory Data Analysis (EDA)
+\n**:green[Solution:]** Compared the sales dataset with the critic and user reviews to determine whether video games have improved as the gaming market has grown. Found answers to several business questions using joins, set theory, grouping, filtering, and ordering techniques.
+\n**:green[Impact:]** The findings from this analysis provides insights to developer team that helps them finalize the Game Design Document during the pre-production stage.
+\n**:green[Tools & Technology:]** Relational databases, PostgreSQL
+\n**:green[Click here for work sample]**
 ''')
 
 
