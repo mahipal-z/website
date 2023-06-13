94c9b6b7cf85306eafe15b8f1a31e0f1a9966622
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-12 22:49:14 -0400

    Update 2_Data_Analysis_and_Business_Intelligence.py

--- pages/2_Data_Analysis_and_Business_I.py
+++ pages/2_Data_Analysis_and_Business_I.py
@@ -1,9 +1,31 @@
 import streamlit as st
-import pandas as pd
 
 st.title("Data Analysis and Business Intelligence")
 
-df1 = pd.read_excel("DA.xlsx", header=None, names=None)
+st.divider()
 
-st.dataframe(data=df1, height=1000, use_container_width=True, hide_index=True, column_config=None)
+st.header(':blue[1. Exploratory data analysis of the top 400 video games released since 1977]')
+st.write('''
+### [1. Exploratory data analysis of the top 400 video games released since 1977](https://github.com/mahipal-z/Videogames-EDA/blob/main/notebook.ipynb)
+----
+<ins>__Problem Description:__</ins> The objective was to do a market research analysis of video games sector by identifying release years that users and critics liked best and exploring the business side of gaming by looking at game sales data.  
+<ins>__Problem Type:__</ins> __Exploratory Data Analysis (EDA)__  
+<ins>__Solution:__</ins> Compared the sales dataset with the critic and user reviews to determine whether video games have improved as the gaming market has grown. Found answers to several business questions using joins, set theory, grouping, filtering, and ordering techniques.  
+<ins>__Impact:__</ins> The findings from this analysis provides insights to developer team that helps them finalize the Game Design Document during the pre-production stage.  
+<ins>__Tools & Technology:__</ins>  __Relational databases__, __PostgreSQL__  
+''')
+
+
+
+
+
+
+
+
+
+#####################################################################################################
+#import pandas as pd
+#df1 = pd.read_excel("DA.xlsx", header=None, names=None)
+
+#st.dataframe(data=df1, height=1000, use_container_width=True, hide_index=True, column_config=None)
 
