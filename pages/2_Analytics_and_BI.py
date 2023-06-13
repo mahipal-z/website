6d2e4505387aed7daa97aa941c796f78c4f32a62
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-13 00:04:24 -0400

    Update 2_Analytics_and_BI.py

--- pages/2_Analytics_and_BI.py
+++ pages/2_Analytics_and_BI.py
@@ -4,11 +4,10 @@ st.header("Data Analysis and Business Intelligence")
 
 st.divider()
 
-#def mz_expander(key):
-#  return st.expander(key, expanded=True)
+#expander = st.expander(expanded=True)
 
-with st.expander(':blue[1. Exploratory data analysis of the top 400 video games]'):
-  st.markdown('''
+with st.expander(':blue[1. Exploratory data analysis of the top 400 video games]', expanded=True):
+    st.markdown('''
 **:green[Problem Description:]** The objective was to do a market research analysis of video games sector by identifying release years that users and critics liked best and exploring the business side of gaming by looking at game sales data.
 \n**:green[Problem Type:]** Exploratory Data Analysis (EDA)
 \n**:green[Solution:]** Compared the sales dataset with the critic and user reviews to determine whether video games have improved as the gaming market has grown. Found answers to several business questions using joins, set theory, grouping, filtering, and ordering techniques.
