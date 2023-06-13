8f4703accde5d9288605105e5fb8ed8fb32a25a3
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-12 23:54:02 -0400

    Update 2_Analytics_and_BI.py

--- pages/2_Analytics_and_BI.py
+++ pages/2_Analytics_and_BI.py
@@ -1,11 +1,15 @@
 import streamlit as st
-
-st.title("Data Analysis and Business Intelligence")
+st.set_page_config(layout="wide")
+st.header("Data Analysis and Business Intelligence")
 
 st.divider()
 
-st.header(':blue[1. Exploratory data analysis of the top 400 video games released since 1977]')
-st.markdown('''
+def mz_expander(key):
+  object = st.expander(key, expanded=True)
+  return object
+
+with mz_expander(':blue[1. Exploratory data analysis of the top 400 video games]'):
+  st.markdown('''
 **:green[Problem Description:]** The objective was to do a market research analysis of video games sector by identifying release years that users and critics liked best and exploring the business side of gaming by looking at game sales data.
 \n**:green[Problem Type:]** Exploratory Data Analysis (EDA)
 \n**:green[Solution:]** Compared the sales dataset with the critic and user reviews to determine whether video games have improved as the gaming market has grown. Found answers to several business questions using joins, set theory, grouping, filtering, and ordering techniques.
