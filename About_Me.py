71fc3bdc38c051cb50055751c34363404d530592
Mahipal Zanakat <117552421+mahipal-z@users.noreply.github.com>
2023-06-15 00:09:07 -0400

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -58,32 +58,27 @@ with col2:
 st.divider()
 
 
-#st.subheader('I could be a good company if')
-st.expander('<p class="big-font">I could be a good company if</p>',expanded=True):
-    st.markdown("""
+st.subheader('I could be a good company if')
+st.markdown("""
 You challenge me to a game of Chess :chess_pawn: [@chess.com](https://friend.chess.com/W7Xra)
 \nor Hiking :runner: nearby or trying a new restaurant :shallow_pan_of_food:
 \nor Bowling :bowling:, Lifting Weights :weight_lifter:, Card games, Board games :game_die:, or for Cooking
 \nor you are willing to teach me Swimming :swimmer:, Golfing :golfer:, Pool :8ball:, or Piano :musical_keyboard:
-""",expanded=True)
+""")
 #    st.markdown()
 
 st.divider()
 
-#st.subheader("Certification")
-
-with st.expander("Certification",expanded=True):
-    st.image([image3,image4,image5], width=200)
-    st.image([image6,image7,image8,image9], width=170)
-    st.markdown("""**Validation Credentials** [**Credly**](https://www.credly.com/users/mahipal-zanakat) | [**DataCamp**](https://www.datacamp.com/certificate/DS0021765360075)""")
+st.subheader("Certification")
+st.image([image3,image4,image5], width=200)
+st.image([image6,image7,image8,image9], width=170)
+st.markdown("""**Validation Credentials** [**Credly**](https://www.credly.com/users/mahipal-zanakat) | [**DataCamp**](https://www.datacamp.com/certificate/DS0021765360075)""")
 
 #components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="8e92d3b3-1cfa-4ad3-8173-a4c06bb239a2" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>"""
 #                ,height=250)
 st.divider()
-#st.subheader("Education")
-
-with st.expander("Education",expanded=True):
-    st.markdown("""**Master of Engineering, Mechanical | University of Ottawa**
+st.subheader("Education")
+st.markdown("""**Master of Engineering, Mechanical | University of Ottawa**
          \n**Data Scientist in Python | DataCamp**
          \n**Data Analyst in SQL | DataCamp**
          \n**Machine Learning by Andrew Ng. | Stanford University**""")
